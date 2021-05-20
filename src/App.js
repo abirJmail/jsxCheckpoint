95c460e71613aa1869442066a15633bac1f415f3
abirJmail <jmailabir@gmail.com>
2021-05-20 10:14:12 +0100

    final upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import women from "./women.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+    <div style={{border:"solid 1px black", maxWidth:"100vw", backgroundColor: "rosybrown",textAlign: "center"}}>
+    <h1 className="title red">ABIR JMAIL</h1>
+    <br />
+    <img src={women} alt ='women' width="500" height="360" />
+    <br />
+    <img src="/download.jpg" width="700" height="360" />
     </div>
+    <video width="700" height="360"  controls >
+      <source src="/girl.mp4" type="video/mp4" />
+    </video>
+</div>
+
+    
   );
 }
 
